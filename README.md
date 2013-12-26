17ea3d23bbf3f50465224717b8feb090884d37a7
Jan Alexander Slabiak <4k3nd0@gmail.com>
2013-12-26 23:05:51 +0100

    Introduce basic daemon configuration, update documentation.
      Introduce the default configuration parameter via puppet template
      system. Extend existing manifest to be able to use parameter.
    
      Update README with some basic instructions.

--- README.md
+++ README.md
@@ -1,10 +1,55 @@
-transmission
+# puppet-transmission #
 
-This is the transmission module.
+![Transmission Logo ](https://upload.wikimedia.org/wikipedia/commons/2/22/Transmission_logo.jpg)
+
+[Transmission ](http://www.transmissionbt.com/) is a fast, easy and free BitTorrent Client. It has different interfaces. This puppet module supports the daemon and configuration, with an additional RPC based web interfaces. But it allows the installation of all different interfaces(gtk+,QT,cli)
 
 [![Build Status](https://travis-ci.org/solarkennedy/puppet-transmission.png)](https://travis-ci.org/solarkennedy/puppet-transmission)
 
-DONT USE THIS YET
+## Installation ##
+
+Getting it locally:
+
+    git clone git@github.com:Akendo/puppet-transmission.git
+
+For a puppetmaster with a `module ` structurer:
+
+    git clone git@github.com:Akendo/puppet-transmission.git module/transmission
+
+
+Note for Transmission-daemon:
+Currently transmission overwrites the settings.json configuration file
+on exit. The daemon can be instructed to reload the config with a SIGHUP,
+or
+    # invoke-rc.d transmission-daemon reload
+
+Otherwise changes made manually to the file while the daemon is running
+will be silently overwritten on exit.
+
+## Usage ##
+
+This will create a basic `transmission-daemon` with enable RPC interfaces.
+User is `torrentt` and password is empty. It will allow everyone host 
+from the 10.1.1.0 network.
+
+      class {'transmission::daemon':
+        rpc_enabled       => true,
+        bind_address_ipv4 => $::ipaddress_eth0,
+        rpc_bind_address  => $::ipaddress_eth0,
+        rpc_username      => "torrent",
+        rpc_whitelist     => "10.1.1.*",
+        rpc_password      => "{a6d7691be9099dedbd410c8566113e386d6623a69huYVwhh"
+      }
+
+
+
+## ToDo ##
+
+ Update Travis tests with new configuration options.
+ More Documentation
+ Cross Platform testing, Ubuntu(13.10) is tested only so far
+ Apply right reload /start /stop feature to allow correct
+
 
 License
 -------
@@ -12,6 +57,8 @@ Apache 2.0
 
 Contact
 -------
+Akendo < 4k3nd0@gmail.com >
+
 Kyle Anderson <kyle@xkyle.com>
 
 Support
--- manifests/daemon/params.pp
+++ manifests/daemon/params.pp
@@ -1,21 +1,85 @@
-class transmission::daemon::params {
-  $service_enable    = true
-  $service_ensure    = 'running'
-  $service_manage    = true
-  $package_ensure    = 'latest'
+  class transmission::daemon::params {
+    $service_enable     = true
+    $service_ensure     = 'running'
+    $service_manage     = true
+    $package_ensure     = 'latest'
 
-  $download_dir = "/var/lib/transmission-daemon/downloads"
-  $incomplete_dir = undef
-  $rpc_url = undef
-  $rpc_port = 9091
-  $rpc_user = "transmission"
-  $rpc_password = undef
-  $rpc_whitelist = undef
-  $blocklist_url = undef
+    $transmission_user  = 'debian-transmission'
+    $transmission_group = 'debian-transmission'
+
+    $alt_speed_down                 = 50
+    $alt_speed_enabled              = false
+    $alt_speed_time_begin           = 540
+    $alt_speed_time_day             = 127
+    $alt_speed_time_enabled         = false
+    $alt_speed_time_end             = 1020
+    $alt_speed_up                   = 50
+    $bind_address_ipv4              = "0.0.0.0"
+    $bind_address_ipv6              = "::"
+    $blocklist_enabled              = false
+    $blocklist_url                  = "http://www.example.com/blocklist"
+    $cache_size_mb                  =  4
+    $dht_enabled                    =  true
+    $download_dir                   =  "/var/lib/transmission-daemon/downloads"
+    $download_limit                 =  100
+    $download_limit_enabled         =  0
+    $download_queue_enabled         =  true
+    $download_queue_size            =  5
+    $encryption                     =  0
+    $idle_seeding_limit             =  30
+    $idle_seeding_limit_enabled     =  false
+    $incomplete_dir                 =  "/home/debian-transmission/Downloads"
+    $incomplete_dir_enabled         =  false
+    $lpd_enabled                    =  false
+    $max_peers_global               =  200
+    $message_level                  =  2
+    $peer_congestion_algorithm      =  ""
+    $peer_id_ttl_hours              =  6
+    $peer_limit_global              =  200
+    $peer_limit_per_torrent         =  50
+    $peer_port                      =  60996
+    $peer_port_random_high          =  65535
+    $peer_port_random_low           =  49152
+    $peer_port_random_on_start      =  false
+    $peer_socket_tos                =  "default"
+    $pex_enabled                    =  true
+    $port_forwarding_enabled        =  true
+    $preallocation                  =  1
+    $prefetch_enabled               =  1
+    $queue_stalled_enabled          =  true
+    $queue_stalled_minutes          =  30
+    $ratio_limit                    =  2
+    $ratio_limit_enabled            =  false
+    $rename_partial_files           =  true
+    $rpc_authentication_required    =  true
+    $rpc_bind_address               =  "0.0.0.0"
+    $rpc_enabled                    =  true
+    $rpc_password                   =  ""
+    $rpc_port                       =  9091
+    $rpc_url                        =  "/transmission/"
+    $rpc_username                   =  ""
+    $rpc_whitelist                  =  ""
+    $rpc_whitelist_enabled          =  true
+    $scrape_paused_torrents_enabled =  true
+    $script_torrent_done_enabled    =  false
+    $script_torrent_done_filename   =  ""
+    $seed_queue_enabled             =  false
+    $seed_queue_size                =  10
+    $speed_limit_down               =  100
+    $speed_limit_down_enabled       =  false
+    $speed_limit_up                 =  100
+    $speed_limit_up_enabled         =  false
+    $start_added_torrents           =  true
+    $trash_original_torrent_files   =  false
+    $umask                          =  18
+    $upload_limit                   =  100
+    $upload_limit_enabled           =  0
+    $upload_slots_per_torrent       =  14
+    $utp_enabled                    =  false
 
   case $::osfamily {
     'Debian','RedHat': {
-      $package_name = [ 'transmission-daemon' ]
+    $package_name = [ 'transmission-daemon' ]
     }
     default: { fail("You asked me to install the transmission-daemon on an OS I don't know about: $::osfamily") }
   }
