dfa31cc54950cb96e1f1e41039a7cab399544305
Jan Alexander Slabiak <4k3nd0@gmail.com>
2014-02-18 23:19:46 +0100

    Create basic test for deamon class.

--- spec/classes/transmission_daemon_spec.rb
+++ spec/classes/transmission_daemon_spec.rb
@@ -1,6 +1,14 @@
 require 'spec_helper'
 
 describe 'transmission::daemon', :type => 'class' do
+	let(:title) { 'daemon' }
+
+	describe "With no parameter" do 
+		it { should create_class("transmission::daemon") }
+
+		  it { should_not contain_class("transmission::config") }
+		  it { should_not contain_class("transmission::param") }
+	end
 
   context "With an unknown OS it should fail" do
     let :facts do { :osfamily => "Unknown" } end
@@ -9,10 +17,9 @@ describe 'transmission::daemon', :type => 'class' do
     }
   end
   
-  context "With Ubuntu/Ubuntu it should install and configure the service" do
+ context "With Ubuntu/Ubuntu it should install the service" do
     let :facts do { :osfamily => "Debian" } end
     it { should contain_class 'transmission::daemon::install' }
-    it { should contain_class 'transmission::daemon::config' }
     it { should contain_class 'transmission::daemon::service' }
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,8 @@
 require 'puppetlabs_spec_helper/module_spec_helper'
 
+require 'rspec-puppet'
+
+RSpec.configure do |c|
+		c.module_path = File.expand_path(File.join(File.dirname(__FILE__), '..', '..'))
+end
+
